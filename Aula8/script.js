d142fd98e025a6114e85e1ecdba33b94bb77fcf3
Jessica Ribas <jessicabarbarasr@gmail.com>
2024-07-26 12:18:43 -0300

    create aulas 7 e 8

---
+++ Aula8/script.js
@@ -0,0 +1,14 @@
+// Comentário de uma linha em JavaScript
+
+/*
+    comentario de varias linhas
+    em JS
+
+*/
+
+let alt, larg, conf;
+alt = parseFloat(prompt("Digite a altura: "));
+larg = parseFloat(prompt("Digite a largura: "));
+conf = confirm("Deseja continuar?? ")
+console.log(alt,larg,conf)
+alert(`${alt} * ${larg} = ${alt*larg}`)
